5b9259b8e3f5e68da8219bb2609ea2040afc090f
Siddhant Prajapati <siddhantprajapati6377@gmail.com>
2024-05-21 11:18:52 +0530

    implemented lazy loading on component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path : 'admin' ,
+        loadChildren : ()=> import('./admin/admin.module').then(module => module.AdminModule)
+    },
+    {
+        path : 'add-user',
+        loadComponent : () => import('./admin/add-user/add-user.component').then(component => component.AddUserComponent)
+    }
+];
